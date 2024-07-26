5f861c16bc606cb94e263671eff8462f18e05fc2
zoeng6project <161369393+zoeng6project@users.noreply.github.com>
2024-07-26 15:08:53 -0400

    Update validate.php

--- inc/validate.php
+++ inc/validate.php
@@ -10,27 +10,23 @@ $password = hash('sha512', $_POST['password']);
 $sql = "SELECT user_id, username, company_code  FROM login 
         WHERE username = '$username' AND password = '$password'";
 $result = $conn->query($sql);
-echo "before count";
+
 // store the number of results in a variable
 $count = $result->rowCount();
 
-echo $count;
 // check if any matches found
 if ($count == 1) {
     $row = $result->fetch();
-	echo $row;
 
     $last_activity = time() ;
-echo $last_activity;
+
     // take the user's id from the database and store it in a session variable
     $_SESSION['user_id'] = $row['user_id'];
     $_SESSION['username'] = $row['username'];
     $_SESSION['company_code'] = $row['company_code'];
     $_SESSION['timeout'] = $last_activity  + 30;
 	$username= $row['username'];
-echo $_SESSION['user_id'] ;
-	echo $_SESSION['username'] ;
-	echo $username ;
+
     // Set cookie
     setcookie('username', $username, $last_activity  +  30, '/');
 
